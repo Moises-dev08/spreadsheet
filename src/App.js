735770055ffe81c1c190286d3cbe53e2bbc7a9c0
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-09-23 12:25:13 -0300

    FEATURE/ routing added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import SpreadSheetScreen from "./screen/spreadSheet/SpreadSheetScreen";
+import HomeScreen from "./screen/home/HomeScreen";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/spreadSheet">
+            <SpreadSheetScreen />
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
