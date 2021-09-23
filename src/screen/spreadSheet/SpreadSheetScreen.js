6bbf97df19e00630d9e187a7f4b164fdf8f3dbb8
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-09-23 12:19:02 -0300

    FEATURE/ screen home and spreadSheet components

---
+++ src/screen/spreadSheet/SpreadSheetScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SpreadSheetComponent from "../../components/SpreadSheet/SpreadSheetComponent";
+
+const SpreadSheetScreen = () => {
+  return <SpreadSheetComponent />;
+};
+
+export default SpreadSheetScreen;
